98f981e21b9fb21819b854debd022da540282bee
Cristhian_Rodriguez <cristhianr.0912@gmail.com>
2022-09-20 18:03:41 -0500

    Authentication and Authorization  with JWT, register a new user

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,21 @@ dotenv.config();
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('<h1 style="color: blue;">API is running...</h1>');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
